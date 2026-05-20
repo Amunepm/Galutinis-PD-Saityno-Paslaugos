43e9db615cc344d3fcf862ba797cdf05c77c0722
Amunepm <amunepm@gmail.com>
2026-05-20 20:47:16 +0300

    upload

---
+++ src/main/java/lt/viko/eif/pmaciulevicius/ggavenas/galutinisprojektas/GalutinisprojektasApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.pmaciulevicius.ggavenas.galutinisprojektas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalutinisprojektasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalutinisprojektasApplication.class, args);
+	}
+
+}
